0266a1761a6ff230824693c2ce37d93058e94460
IMANUELQS <imanuel_qs@hotmail.com>
2024-04-21 11:27:19 -0600

    Committ prueba para subir archivo a GitHub

---
+++ src/repaso.kt
@@ -0,0 +1,14 @@
+fun main(args:Array<String>){
+
+println ("Ingresa primer número")
+
+    var a:Int = readLine()?.toInt()?:0
+
+    println ("Ingresa segundo número")
+
+    var b:Int = readLine()?.toInt()?:0
+
+    println (a+b)
+
+
+}
